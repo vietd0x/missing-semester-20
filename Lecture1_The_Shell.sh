63863ecb631081729f0f79b05b0fefd1643eeec8
vietd0x <76168059+vietd0x@users.noreply.github.com>
2021-02-12 17:54:43 +0700

    Create Lecture1_The_Shell.sh

---
+++ Lecture1_The_Shell.sh
@@ -0,0 +1,15 @@
+echo Hello\ World
+# Hello World
+echo $PATH
+which echo
+# usr/bin/echo
+# linux & macOS these paths are separeted by slashes forward- /, on windows paths are usually separated by back slashes instead
+pwd # present working dir
+cd /home
+pwd
+# /home
+# . mean the current dir, .. mean the parent dir
+cd ..
+pwd
+# /
+cd ./home
