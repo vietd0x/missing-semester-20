e381b18e2177b2e1686707d8c65f8a7a6e258ee5
vietd0x <76168059+vietd0x@users.noreply.github.com>
2021-02-13 23:58:44 +0700

    Update Lecture1_The_Shell.sh

--- Lecture1_The_Shell.sh
+++ Lecture1_The_Shell.sh
@@ -13,3 +13,8 @@ cd ..
 pwd
 # /
 cd ./home
+cd ~ # bring you to home dir
+cd - # it will cd to the directory u were PREVIOUSLY IN
+# r- mean u see which files are inside this dir as lists for a dir or list this content
+# w- allowed to rename, creat or remove files within that dir
+# if u have write permissions on a file, but u dont have write permissions on its dir, you cannot delete the file, u only can empty it 
